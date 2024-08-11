f400888541e10c09b731b8f9da56fff86abe854e
Kuldeep Kushwaha <159529497+kuldeep162003@users.noreply.github.com>
2024-08-11 20:11:44 +0530

    added loding spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import "./Spinner.css"
+
+const Spinner = () => {
+    return (
+        <div className="w-full flex justify-center translate-y-24">
+            <div class="spinner"></div>
+        </div>
+        
+        
+    );
+}
+
+export default Spinner;
