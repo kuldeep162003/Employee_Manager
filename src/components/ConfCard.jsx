a342e712cc0ff578e46874ddb2afa21522f7ec3d
Kuldeep Kushwaha <159529497+kuldeep162003@users.noreply.github.com>
2024-08-12 16:51:15 +0530

    Update ConfCard.jsx

--- src/components/ConfCard.jsx
+++ src/components/ConfCard.jsx
@@ -7,7 +7,7 @@ function ConfCard({confirmDeleteHandler, cancelDeleteHandler}){
             <div className="bg-gray-200 absolute flex flex-col justify-center items-center px-10 py-5 rounded-md top-[50%] left-[50%] -translate-x-[50%] -translate-y-[70%]">
                 <MdErrorOutline className="text-orange-400 text-4xl font-extralight mb-2"/>
                 <p className="font-semibold text-2xl">Are you sure?</p>
-                <p className="mb-4">You won't be able to revert this!</p>
+                <p className="mb-4 text-center">You won't be able to revert this!</p>
                 <div className="w-full flex justify-between">
                     <button className="bg-blue-500 text-gray-100 font-semibold px-4 py-1 rounded-md" onClick={confirmDeleteHandler}>Delete</button>
                     <button className="bg-red-500 text-gray-100 font-semibold px-4 py-1 rounded-md" onClick={cancelDeleteHandler}>Cancel</button>
