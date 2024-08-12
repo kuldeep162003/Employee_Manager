274e31d1a1a00ec021e009ac7f679f25342c93a5
Kuldeep Kushwaha <159529497+kuldeep162003@users.noreply.github.com>
2024-08-12 18:53:17 +0530

    Update SubmitConf.jsx

--- src/components/SubmitConf.jsx
+++ src/components/SubmitConf.jsx
@@ -7,7 +7,7 @@ function SubmitConf({confirmHandler, cancelHandler}){
             <div className="bg-gray-200 absolute flex flex-col justify-center items-center px-10 py-5 rounded-md top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
                 <IoMdCheckmarkCircleOutline className="text-green-400 text-4xl font-extralight mb-2"/>
                 <p className="font-semibold text-2xl text-black">Submit data?</p>
-                <p className="mb-4 text-black">Please confirm for the data submission!</p>
+                <p className="mb-4 text-black text-center">Please confirm for the data submission!</p>
                 <div className="flex gap-4">
                     <button className="bg-green-500 text-gray-100 font-semibold px-4 py-1 rounded-md" onClick={confirmHandler}>Yes, Submit!</button>
                     <button className="bg-gray-300 text-gray-800 font-semibold px-4 py-1 rounded-md" onClick={cancelHandler}>Cancel</button>
